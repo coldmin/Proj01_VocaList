d48d3d68475ed710466a1b51159351876060a849
cold Ahn <Coldmin@DESKTOP-K2MPU10>
2023-04-18 02:49:20 +0900

    Menu updated

--- Proj01_VocaList/src/menu/Menu.java
+++ Proj01_VocaList/src/menu/Menu.java
@@ -6,7 +6,7 @@ public class Menu {
 		System.out.println();
 		System.out.println("▶ 우리말 단어장 ◀");
 		System.out.println();
-		System.out.println("[단어장 관리 명령어 사용법]");
+		System.out.println("[[단어장 관리 명령어 사용법]]");
 		System.out.println("add - 단어 추가");
 		System.out.println("del - 단어 삭제");
 		System.out.println("edit - 단어 수정");
