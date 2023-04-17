94dd82c49bb6fc45fb0d9e70310cb491bf1c4415
cold Ahn <Coldmin@DESKTOP-K2MPU10>
2023-04-18 03:13:40 +0900

    Menu updated

--- Proj01_VocaList/src/menu/Menu.java
+++ Proj01_VocaList/src/menu/Menu.java
@@ -6,14 +6,14 @@ public class Menu {
 		System.out.println();
 		System.out.println("▶ 우리말 단어장 ◀");
 		System.out.println();
-		System.out.println("ㅣ단어장 관리 명령어 사용법ㅣ");
-		System.out.println("add - 단어 추가");
-		System.out.println("del - 단어 삭제");
-		System.out.println("edit - 단어 수정");
-		System.out.println("list - 전체 목록");
-		System.out.println("list_name - 가나다순 정렬");
-		System.out.println("list_date - 날짜순 정렬");
-		System.out.println("exit - 종료");
+		System.out.println("[단어장 관리 명령어 사용법]");
+		System.out.println(" 1. add - 단어 추가");
+		System.out.println(" 2. del - 단어 삭제");
+		System.out.println(" 3. edit - 단어 수정");
+		System.out.println(" 4. list - 전체 목록");
+		System.out.println(" 5. list_name - 가나다순 정렬");
+		System.out.println(" 6. list_date - 날짜순 정렬");
+		System.out.println(" 7. exit - 종료");
 		
 	}
 	public static void prompt() {
